a9962eec50150b2268aae0d9e36aea27677e7ac4
Timo H <timo@dealmachine.net>
2011-07-11 19:57:33 +0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Laitevuokraus::Application.config.secret_token = '07cb5989b095b0609a664a631b7050b9e3c5422c029e4929f40d30f5742c4f4990de98a2e95c4ea449ddbcd827902e21f67d6972dd556e1d2339acab9b5f50af'
